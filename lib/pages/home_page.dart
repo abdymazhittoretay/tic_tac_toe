dd15c2ee22162d9db4fa429d3c435929a120edd0
Toretay <abdymvzhit@gmail.com>
2024-12-31 13:55:58 +0500

    added style in ticTacToe and buttons now work

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,10 +9,10 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<String> gridList = [
-    "x",
-    "o",
-    "x",
-    "o",
+    "",
+    "",
+    "",
+    "",
     "",
     "",
     "",
@@ -28,53 +28,73 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.all(24.0),
-              margin: EdgeInsets.symmetric(horizontal: 10.0),
-              decoration: BoxDecoration(
-                  color: Colors.blue[100],
-                  borderRadius: BorderRadius.circular(10.0)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text(
-                    "Player 1 Score: $player1Score",
-                    style: TextStyle(color: Colors.black, fontSize: 16),
-                  ),
-                  Text(
-                    "Player 2 Score: $player2Score",
-                    style: TextStyle(color: Colors.black, fontSize: 16),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Expanded(
-                child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10.0),
-              child: GridView.builder(
-                itemCount: gridList.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3),
-                itemBuilder: (context, index) {
-                  return Container(
-                    decoration:
-                        BoxDecoration(border: Border.all(color: Colors.black)),
-                    child: Center(
-                      child: Text(
-                        gridList[index],
-                        style: TextStyle(color: Colors.black, fontSize: 52.0),
-                      ),
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(24.0),
+                margin: EdgeInsets.symmetric(horizontal: 10.0),
+                decoration: BoxDecoration(
+                    color: Colors.blue[100],
+                    borderRadius: BorderRadius.circular(10.0)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      "Player 1 Score: $player1Score",
+                      style: TextStyle(color: Colors.black, fontSize: 16),
                     ),
-                  );
-                },
+                    Text(
+                      "Player 2 Score: $player2Score",
+                      style: TextStyle(color: Colors.black, fontSize: 16),
+                    ),
+                  ],
+                ),
               ),
-            ))
-          ],
+              SizedBox(
+                height: 10.0,
+              ),
+              Expanded(
+                  child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 10.0),
+                padding: const EdgeInsets.all(24.0),
+                decoration: BoxDecoration(
+                    color: Colors.blue[100],
+                    borderRadius: BorderRadius.circular(10.0)),
+                child: GridView.builder(
+                  itemCount: gridList.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3),
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          if (xTurn && gridList[index] == "") {
+                            gridList[index] = "x";
+                            xTurn = !xTurn;
+                          } else if (!xTurn && gridList[index] == "") {
+                            gridList[index] = "o";
+                            xTurn = !xTurn;
+                          }
+                        });
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black)),
+                        child: Center(
+                          child: Text(
+                            gridList[index],
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 52.0),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ))
+            ],
+          ),
         ),
       ),
     );
